a9ef9910a3017fa48ed4376936cd59e640ff1bac
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-03-13 13:26:45 -0600

    New array and object methods, made function that eliminates duplicates in an array

--- sugarplum.js
+++ sugarplum.js
@@ -20,6 +20,58 @@ deleteItem(shoppingList, `Sushi`)
 addItem(shoppingList, `Jerky`);
 
 console.log(shoppingList)
-    
 
+const names = [`Suzie`, `Ben`, `Mark`, `Franklin`, `Ben`, `Sarah`, `Mark`];
 
+var namesUnique = names.filter(function(item, index){
+    return names.indexOf(item) >= index;
+});
+
+console.log(namesUnique)
+
+const employees = {
+    tom: `designer`,
+    chris: `engineer`,
+    james: `developer`
+}
+
+const employeeNames = [];
+const employeeRoles = [];
+
+for (var prop in employees) {
+    employeeNames.push(prop);
+}
+
+for (var prop in employees) {
+    employeeRoles.push(employees[prop])
+}
+
+console.log(employeeNames);
+console.log(employeeRoles);
+
+const payRoll = {
+    sonya: 300,
+    estrid: 340,
+    kris: 1300,
+    sarah: 23
+}
+
+for (var prop in payRoll) {
+    payRoll[prop] += 100;
+}
+
+console.log(payRoll)
+
+var backpack = {
+    oldLaptop: `slow`,
+    oldLunch: `moldy`,
+    pencil: `sharp`
+};
+
+const oldBackPackStuff = [`oldLaptop`, `oldLunch`];
+
+for (let i = 0; i < oldBackPackStuff.length; i++) {
+    delete backpack[oldBackPackStuff[i]];
+}
+
+console.log(backpack)
